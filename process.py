62b9be9e936dfa96da9c9b63978a0456ca91ecd9
leonshen95 <leyang.shen@uconn.edu>
2018-10-28 15:52:11 -0400

    Create train and test.txt

---
+++ process.py
@@ -0,0 +1,48 @@
+# import glob, os
+#
+# # Current directory
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+#
+# # Directory where the data will reside, relative to 'darknet.exe'
+# path_data = 'test_data/'
+#
+# # Percentage of images to be used for the test set
+# percentage_test = 10
+#
+# # Create and/or truncate train.txt and test.txt
+# file_train = open('train.txt', 'w')
+# file_test = open('test.txt', 'w')
+#
+# # Populate train.txt and test.txt
+# counter = 1
+# index_test = round(100 / percentage_test)
+# for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+#     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+#
+#     if counter == index_test:
+#         counter = 1
+#         file_test.write(path_data + title + '.jpg' + "\n")
+#     else:
+#         file_train.write(path_data + title + '.jpg' + "\n")
+#         counter = counter + 1
+import glob, os
+# Current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print(current_dir)
+current_dir = '/Users/leon/darknet/test_data'
+# Percentage of images to be used for the test set
+percentage_test = 10;
+# Create and/or truncate train.txt and test.txt
+file_train = open('train.txt', 'w')
+file_test = open('test.txt', 'w')
+# Populate train.txt and test.txt
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(current_dir + "/" + title + '.jpg' + "\n")
+    else:
+        file_train.write(current_dir + "/" + title + '.jpg' + "\n")
+        counter = counter + 1
