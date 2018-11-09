e681d1169ea5a713a6c6ee8f2ff97e0e5266de9f
Kainan Liu <36947512+liuknan@users.noreply.github.com>
2018-11-09 12:42:08 -0500

    Update test.py

--- get_image/test.py
+++ get_image/test.py
@@ -1,2 +1,2 @@
 import urllib.request
-urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location=38.06344466%2C-84.52000672&heading=42.71109980359694&pitch=5&fov=80&key=AIzaSyAgfIHLW-ZOWqtEMQE_aC42ZBHZ6YhU_Fo")
+urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location=38.06344466%2C-84.52000672&heading=42.71109980359694&pitch=5&fov=80&key=")
