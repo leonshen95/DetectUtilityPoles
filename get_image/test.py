d2c005f5be46dd8030904a3979b0023d752d61f5
liuknan <liuknan@gmail.com>
2018-11-10 01:36:10 -0500

    new classify

--- get_image/test.py
+++ get_image/test.py
@@ -1,5 +1,17 @@
 import urllib.request
-<<<<<<< HEAD
+import subprocess
+def runprogram(program, args, inputstr):
+    coll_run = subprocess.run(
+        [program, *args],
+        input=inputstr.encode(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+    ret_code = coll_run.returncode
+    program_output = coll_run.stdout.decode()
+    program_errors = coll_run.stderr.decode()
+    return (ret_code, program_output, program_errors)
+program="./classify_image.py"
 heading=0
 num=0
 a="38.06344466"
@@ -7,6 +19,8 @@ b="-84.52000672"
 for i in range(1,7):
     urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location="+a+","+b+"&heading="+str(heading)+"&key=AIzaSyAgfIHLW-ZOWqtEMQE_aC42ZBHZ6YhU_Fo",str(i)+".jpg")
     heading=heading+60
-=======
-urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location=38.06344466%2C-84.52000672&heading=42.71109980359694&pitch=5&fov=80&key=")
->>>>>>> e681d1169ea5a713a6c6ee8f2ff97e0e5266de9f
+
+for i in range(1,7):
+    subprocess.run([program,"--image_file",str(i)+".jpg"])
+
+
