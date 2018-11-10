2a5f238a8fa7623c93c14ab030764bb218d72ea8
liuknan <liuknan@gmail.com>
2018-11-09 23:13:45 -0500

    test

--- get_image/test.py
+++ get_image/test.py
@@ -1,2 +1,8 @@
 import urllib.request
-urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location=38.06344466%2C-84.52000672&heading=42.71109980359694&pitch=5&fov=80&key=AIzaSyAgfIHLW-ZOWqtEMQE_aC42ZBHZ6YhU_Fo")
+heading=0
+num=0
+a="38.06344466"
+b="-84.52000672"
+for i in range(1,7):
+    urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location="+a+","+b+"&heading="+str(heading)+"&key=AIzaSyAgfIHLW-ZOWqtEMQE_aC42ZBHZ6YhU_Fo",str(i)+".jpg")
+    heading=heading+60
