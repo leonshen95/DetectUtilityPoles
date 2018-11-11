41fe548f205824090fc34ac9f6a354101cc31f18
liuknan <liuknan@gmail.com>
2018-11-11 11:59:35 -0500

    new

--- get_image/classify_image.py
+++ get_image/classify_image.py
@@ -165,10 +165,11 @@ def run_inference_on_image(image):
     top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
     for node_id in top_k:
       human_string = node_lookup.id_to_string(node_id)
-      if "pole" in human_string:
+      if ("pole" in human_string) and ("flagpole" not in human_string):
         polelist.append(image)
+        polelist.append(predictions[node_id])
       score = predictions[node_id]
-      # print('%s (score = %.5f)' % (human_string, score))
+      print('%s (score = %.5f)' % (human_string, score))
 
 
 # def maybe_download_and_extract():
@@ -192,9 +193,10 @@ def run_inference_on_image(image):
 
 def main(_):
   # maybe_download_and_extract()
-  image = (FLAGS.image_file if FLAGS.image_file else
-           os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
-  run_inference_on_image(image)
+  for number in range(1,7):
+    image = (FLAGS.image_file if FLAGS.image_file else
+             os.path.join(FLAGS.model_dir, str(number)+'.jpg'))
+    run_inference_on_image(image)
   if len(polelist):
     print(polelist)
 
--- get_image/test.py
+++ get_image/test.py
@@ -1,28 +1,19 @@
 import classify_image
 import urllib.request
 import subprocess
-def runprogram(program, args, inputstr):
-    coll_run = subprocess.run(
-        [program, *args],
-        input=inputstr.encode(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
 
-    ret_code = coll_run.returncode
-    program_output = coll_run.stdout.decode()
-    program_errors = coll_run.stderr.decode()
-    return (ret_code, program_output, program_errors)
 program="./classify_image.py"
 heading=0
 num=0
-a="42.379653"
-b="-71.124635"
+a="42.348546"
+b="-71.106549"
 for i in range(1,7):
     urllib.request.urlretrieve("https://maps.googleapis.com/maps/api/streetview?size=640x640&location="+a+","+b+"&heading="+str(heading)+"&key=",str(i)+".jpg")
     heading=heading+60
 
-for i in range(1,7):
-    subprocess.run([program,"--image_file",str(i)+".jpg"])
+# for i in range(1,7):
+#     subprocess.run([program,"--image_file",str(i)+".jpg"])
+subprocess.run([program])
 
 
 
