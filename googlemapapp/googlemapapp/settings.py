8842cf18a2a00ef009edfa0b85ff705cf4fe8b65
Fuyao Wang <fuyao@bu.edu>
2018-12-12 22:32:49 -0500

    Update settings.py

--- googlemapapp/googlemapapp/settings.py
+++ googlemapapp/googlemapapp/settings.py
@@ -60,10 +60,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
