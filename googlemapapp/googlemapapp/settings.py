3bfd13c308c97f6780b9937b800f8e96ebae8c4a
Fuyao Wang <fuyao@bu.edu>
2018-12-12 22:33:14 -0500

    Update settings.py

--- googlemapapp/googlemapapp/settings.py
+++ googlemapapp/googlemapapp/settings.py
@@ -85,10 +85,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
